a9bc7963b4fddf26f9641757c96996f92dfca685
LouisDumont <dumont.louis21@gmail.com>
2020-11-21 14:57:00 +0100

    feat: use main.py in command line

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 import os
+import argparse
 
 from LatexParser import LatexParser
 from PageGenerator import PageGenerator
 
 TEX_PATH = os.getcwd() + "/main.tex"
 
-if __name__ == "__main__":
-    latex_parser = LatexParser()
-    page_generator = PageGenerator()
-    latex_parser.parse_latex_custom(TEX_PATH)
-    parsed_text = latex_parser.return_parsed_text()
-    page_generator.process_text(parsed_text)
-    page_generator.create_page("test2.html")
+parser = argparse.ArgumentParser()
+parser.add_argument('--tex_path', help='path to the .tex file to process')
+parser.add_argument('--result_name',
+                    help='Name of the result page (do not forget to add .html extension')
+
+args = parser.parse_args()
+
+TEX_PATH = os.getcwd() + "/" + args.tex_path
+PAGE_NAME = args.result_name
+
+latex_parser = LatexParser()
+page_generator = PageGenerator()
+latex_parser.parse_latex_custom(TEX_PATH)
+parsed_text = latex_parser.return_parsed_text()
+page_generator.process_text(parsed_text)
+page_generator.create_page(PAGE_NAME)
